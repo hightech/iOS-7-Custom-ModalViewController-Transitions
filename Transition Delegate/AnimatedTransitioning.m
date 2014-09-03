e6c2e86b064d211712860b683d32230f5d4a67e0
Anthony Detamore <anthonydetamore@gmail.com>
2014-09-03 13:06:14 -0400

    Update AnimatedTransitioning.m

--- Transition Delegate/AnimatedTransitioning.m
+++ Transition Delegate/AnimatedTransitioning.m
@@ -3,12 +3,11 @@
 //  CustomTransitionExample
 //
 //  Created by Blanche Faur on 10/24/13.
+//  Updated by Anthony Detamore on 9/03/14
 //  Copyright (c) 2013 Blanche Faur. All rights reserved.
 //
 
 #import "AnimatedTransitioning.h"
-#import "MainViewController.h"
-#import "SecondViewController.h"
 
 @implementation AnimatedTransitioning
 
@@ -20,21 +19,44 @@
     return 0.25f;
 }
 
-- (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
+- (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext
+{
+    UIViewController* toVC = (UIViewController*)[transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController* fromVC = (UIViewController *)[transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     
-    UIView *inView = [transitionContext containerView];
-    SecondViewController *toVC = (SecondViewController *)[transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    MainViewController *fromVC = (MainViewController *)[transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    if(self.reverse) {
+        [self executeReverseAnimation:transitionContext fromVC:fromVC toVC:toVC];
+    } else {
+        [self executeForwardAnimation:transitionContext fromVC:fromVC toVC:toVC];
+    }
     
-    [inView addSubview:toVC.view];
+}
+
+-(void)executeForwardAnimation:(id<UIViewControllerContextTransitioning>)transitionContext fromVC:(UIViewController *)fromVC toVC:(UIViewController *)toVC
+{
+    [[transitionContext containerView] addSubview:toVC.view];
     
     CGRect screenRect = [[UIScreen mainScreen] bounds];
     [toVC.view setFrame:CGRectMake(0, screenRect.size.height, fromVC.view.frame.size.width, fromVC.view.frame.size.height)];
     
     [UIView animateWithDuration:0.25f
                      animations:^{
-                         
                          [toVC.view setFrame:CGRectMake(0, 0, fromVC.view.frame.size.width, fromVC.view.frame.size.height)];
+                     }
+                     completion:^(BOOL finished) {
+                         [transitionContext completeTransition:YES];
+                     }];
+}
+
+-(void)executeReverseAnimation:(id<UIViewControllerContextTransitioning>)transitionContext fromVC:(UIViewController *)fromVC toVC:(UIViewController *)toVC
+{
+    [[transitionContext containerView] insertSubview:toVC.view belowSubview:fromVC.view];
+    
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    
+    [UIView animateWithDuration:0.25f
+                     animations:^{
+                         [fromVC.view setFrame:CGRectMake(0, screenRect.size.height, fromVC.view.frame.size.width, fromVC.view.frame.size.height)];
                      }
                      completion:^(BOOL finished) {
                          [transitionContext completeTransition:YES];
